2e6de09bb52ac4db5c46308bbce861e757607d36
Evan <530788697@qq.com>
2019-06-18 22:52:27 +0800

    init

---
+++ src/main/java/com/itmuch/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.itmuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author evanYang
+ * @version 1.0
+ * @date 2019/06/18 22:29
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
